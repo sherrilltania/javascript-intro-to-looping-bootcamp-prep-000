62d167732f6a43373238f448aea68b5a1800559d
Tatiana Sherrill <anilorac_sol@yahoo.com>
2018-07-23 10:37:17 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -15,3 +15,12 @@ function whileLoop(n){
   }
   return "done"
 }
+
+function doWhileLoop(array){
+  function maybeTrue() {
+  return Math.random() >= 0.5
+}
+  do {
+    console.log(array.pop())
+  } while(array.length > 0 && maybeTrue())
+}
